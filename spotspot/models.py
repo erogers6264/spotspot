b6af96350a2073f9a920ddf6da5a2bf69306210e
Ethan Rogers <erogers6264@gmail.com>
2016-01-23 15:11:49 -0700

    Set up db schema

---
+++ spotspot/models.py
@@ -0,0 +1,40 @@
+import sys
+
+from sqlalchemy import Table, Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+associate_lot_destination = Table('lot_destination', Base.metadata,
+    Column('lot_id', Integer, ForeignKey('lot.id')),
+    Column('destination_id', Integer, ForeignKey('destination.id'))
+)
+
+class Lot(Base):
+	"""A simple classsc for the parking lots in the database"""
+	
+	__tablename__ = 'lot'
+	id = Column(Integer, primary_key = True)
+	address = Column(String(250))
+	capacity = Column(Integer)
+	fill_level = Column(Integer, default = 0)
+	image_url = Column(String)
+	destinations = relationship("Destination",
+		secondary = associate_lot_destination,
+		back_populates = 'lots')
+
+
+class Destination(Base):
+	"""Information about a space"""
+	
+	__tablename__ = 'destination'
+	id = Column(Integer, primary_key = True)
+	address = Column(String)
+	lots = relationship("Lot",
+		secondary = associate_lot_destination,
+		back_populates = 'destinations') 
+
+engine = create_engine('sqlite:///lots.db')
+Base.metadata.create_all(engine)
