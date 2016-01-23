29fe403cfb33c8ecde1c0fe1b1c84279b45ee600
Ethan Rogers <erogers6264@gmail.com>
2016-01-23 15:12:21 -0700

    Add more views

--- spotspot/views.py
+++ spotspot/views.py
@@ -3,15 +3,25 @@ from spotspot import app
 
 
 @app.route('/')
-@app.route('/address/')
+@app.route('/destination/')
 def enterAddress():
     return "This page will let the user enter their address"
 
-@app.route('/map/user/')
-def showMap():
-    return "This page will show a map with the user"
+@app.route('/map/<destination_id>/')
+def showMap(destination_id):
+    return "This page will show a map with the user surrounded by lots"
+
+@app.route('/lots/')
+def allLots():
+    return "This page will show all lots in a nice list view"
 
 @app.route('/lot/<int:lot_id>/')
 @app.route('/lot/<int:lot_id>/info/')
 def lotInfo(lot_id):
     return "This page will show info about a lot with an ID of %r" % lot_id
+
+@app.route('/lot/new/')
+def newLot():
+    return "This page will let you add a new parking lot"
+
+
