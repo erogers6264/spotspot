dd4c33b937f90501a696b06d51e823b065b9b110
Ethan Rogers <erogers6264@gmail.com>
2016-01-24 01:58:26 -0700

    Add address table

--- spotspot/models.py
+++ spotspot/models.py
@@ -1,6 +1,6 @@
 import sys
 
-from sqlalchemy import Table, Column, ForeignKey, Integer, String
+from sqlalchemy import Table, Column, ForeignKey, Integer, String, Float
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -14,10 +14,20 @@ class Lot(Base):
 	__tablename__ = 'lot'
 	id = Column(Integer, primary_key = True)
 	address = Column(String)
+	lat = Column(Float)
+	lng = Column(Float)
 	capacity = Column(Integer)
 	fill_level = Column(Integer, default = 0)
 	image_url = Column(String)
 
+class Destination(Base):
+	"""To store address queries made against the program"""
+
+	__tablename__ = 'address'
+	id = Column(Integer, primary_key = True)
+	address = Column(String)
+	lat = Column(Float)
+	lng = Column(Float)
 
 engine = create_engine('sqlite:///lots.db')
 Base.metadata.create_all(engine)
